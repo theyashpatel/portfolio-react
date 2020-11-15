82375b63eb775f444e3ba8a0ee72ae3632399591
Yash <yashpatel26@gmail.com>
2020-11-15 00:08:37 -0500

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            quisquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            quisquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different payment methods?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            quisquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            quisquam.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 export default function AboutUs() {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 }

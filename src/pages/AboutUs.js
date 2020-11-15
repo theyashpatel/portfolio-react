7a2b05586192cf015a3875ccdccdd8661703c389
Yash <yashpatel26@gmail.com>
2020-11-14 23:19:20 -0500

    Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,12 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 export default function AboutUs() {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 }
